92f75f49387afa47b863d8edd23b3ae952731002
ruslanzalikov <rus.zalikov@bk.ru>
2021-12-24 19:00:48 +0300

    beta v 0.0.3

--- test.py
+++ test.py
@@ -6,6 +6,7 @@ kx = 0; ky = 0
 kxx = 1; kyy = 1
 white = (255, 255, 255)
 black = (0, 0, 0)
+green = (0, 255, 0)
 
 pg.init()
 pg.display.set_caption("Spacegame")
@@ -14,6 +15,8 @@ gameScreen.fill((black))
 runGame = True
 pg.display.update()
 k = False
+pg.draw.rect(gameScreen, white, (160, 90, 320, 180))
+pg.display.update()
 while runGame:
     centralx = circ.radius
     centraly = circ.radius
@@ -37,5 +40,5 @@ while runGame:
             ky -= circ.radius
     if k:
         pg.draw.circle(gameScreen, black, (centralx + kx - kxx, centraly + ky - kyy), circ.radius)
-        pg.draw.circle(gameScreen, white, (centralx + kx, centraly + ky), circ.radius)
+        pg.draw.circle(gameScreen, green, (centralx + kx, centraly + ky), circ.radius)
         pg.display.update()
